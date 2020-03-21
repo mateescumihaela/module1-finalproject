04c5c25a5fb2b1bef3ebf77943f304df3c888a9f
Mihaela Mateescu <miha.mateescu@gmail.com>
2020-03-21 00:34:08 +0000

    basic functionality complete

--- js/game.js
+++ js/game.js
@@ -1,178 +1,84 @@
-class Game {
-    constructor (page, ctx, canvas, wordList, input) {
-        this.page = page;
-        this.ctx = ctx;
-        this.canvas = canvas;
-        this.wordList = wordList;
-        this.input = input;
-
-        this.player = new Player (ctx, canvas);
-        this.dictionary = new Dictionary ();
-
-        this.cars = {};
-        this.carsCount = 0;
-        this.counter = 0;
-        this.notCollided = true;
-        this.inputTimer = 0;
-        this.typeStart = 0;
-        this.typeEnd = 0;
-
-        this.drawMenuBackground = this.drawMenuBackground.bind(this);
-        this.resetGame = this.resetGame.bind(this);
-        this.startTimer = this.startTimer.bind(this);
-        this.handleCars = this.handleCars.bind(this);
-        this.startGame = this.startGame.bind(this);
-        this.render = this.render.bind(this);
-        this.gameOver = this.gameOver.bind(this);
-
+let gameManager = (function(){
+    //number to track game state. checked with every keypress
+    //states;
+    // 0-paused, no input, no frame refresh (only PEWs)
+    // 1-opening/'game over' screen. input -> gameManager.newGame
+    // 2-deck rolling. standard gameplay. input -> deck.compare
+    // 3-quiz display. input -> quiz.take
+    // 4-powerAttack display. no input.
+    let state = 1;
+    let intervalId = null;
+    
+    return {
+     getState: function(){
+      return state;
+     },
+     setState: function(s){
+    },
+     newGame: function(){
+    //reset all variables/level/score/DECK[]/pew/background
+      scoreKeeper.clear();
+      scoreKeeper.newLevel();
+      deck.reset();
+      pew_pool = [];
+    //make a new DECK and start gameLoop
+      line_pool.initDeck();
+      state = 2;
+      intervalId = setInterval(gameLoop, 1000/30);
+     },
+     endGame: function(){
+    //add points and bonuses. display result. end gameLoop
+      scoreKeeper.endLevel();
+      ctx.textAlign = 'center';
+      ctx.fillText("Game Over", 400, 150);
+      ctx.fillText("End Score: " + scoreKeeper.getScore(), 400, 200);
+      ctx.fillText("(press any key to start again)", 400, 580);
+      clearInterval(intervalId);
+      state = 0;
+    //wrapped to prevent fast typing from resetting a new game
+      setTimeout(function(){state = 1;}, 2000);
+     }
+    };
+   })();
+   
+   
+// GAME LOOP
+   
+   function gameLoop(){
+    let state = gameManager.getState();
+    //clear canvas. default styles
+    cleanSlate();
+     
+    //if DECK reaches the top of the screen Or reached level 5
+    // end the game loop and display 'game over'/final score
+    //**changing number of levels, also change scoreKeeper.endLevel
+    if(deck.getY() < 85 || scoreKeeper.getLevel() > 4){
+     gameManager.endGame();
+     return;
     }
-
-drawMenuBackground() {
-    this.ctx.beginPath();
-    this.ctx.rect(0, 0, canvas.width, canvas.height);
-    this.ctx.fillStyle = "rgba (0, 0 ,0, 0.8)";
-    this.ctx.fill();
-    this.ctx.closePath();
-}
-
-resetGame() {
-    this.cars = {};
-    this.carsCount = 0;
-    this.counter = 0;
-    this.round = 1;
-    this.notCollided = true;
-    this.player.avoidedCollissionCount = 0;
-}
-
-startTimer(e) {
-if (this.typeStart === 0 && e.target.value != " ") {
-    this.typeStart = Date.now();
-}
-}
-
-
-handleCars(e) {
-    if (e.keyCode === 32 || e.keyCode === 13) {
-      let value = this.input.value.trim();
-      for (let car in this.cars) {
-        if (value === this.cars[car].word) {
-          this.attackTimer = this.counter;
-          this.player.attack = true;
-          this.player.killCount += 1;
-          this.cars[car].word = null;
-          this.cars[car].notCollided = false;
-          break;
-        }
-      }
-      this.input.value = "";
-      if (this.typeStart > 0) {
-        this.typeEnd = Date.now();
-        this.inputTimer += (this.typeEnd - this.typeStart)/1000;
-      }
-      this.typeStart = 0;
-    } 
-  }
-
-startGame(e) {
-    if (e.keyCode === 13 || e.button === 0) {
-        this.canvas.removeEventListener('click', this.startGame);
-        this.page.removeEventListener('click', this.startGame);
-        this.resetGame;
-        clearInterval(window.setInterval);
-        clearInterval(window.overInterval);
-        this.canvas.className = "game-screen";
-        requestAnimationFrame(this.render)
-        this.input.disabled = false;
-        this.input.style.display = "block";
-        this.input.focus();
-
-        }
-
-        }
-
- render() {
-     let request = requestAnimationFrame(this.render);
-     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-     this.canvas.addEventListener('click', this.input.focus());
-     this.input.addEventListener('keydown', this.handleCar);
-     this.input.addEventListener('input', this.startTimer);    
-
-    let fps = 12;
-    let interval = 1000/fps;
-    let now = Date.now();
-    let delta = npw - this.then;
-    setInterval(() => {
-      this.counter += 10
-    }, 5)
-
-    if (this.counter % 10000 === 0) {
-      this.round += .5
+    //state: deck rolling/standard gameplay
+    if(state === 2){
+    //progress the DECK towards the top of the screen and display
+    // LINEs, score, and avatars
+     scoreKeeper.draw();
+     deck.march();
+     deck.draw();
+     fighter.draw(deck.getX() - 100, deck.getY() - 100, 100);
+    //state: powerAttack display
+    } else if(state === 4){ //set to 4
+    
+    //lower the DECK based on score Or nearness to top. Clear quiz.success when power is gone. need a minimum power amount for 1 iteration of animation
+     powerAttack.callPower();
+     deck.draw();
+    //display power animation
+     fighter.draw(300, 200, 200 - powerAttack.getPower());
     }
-    this.player.drawWordList(this.zombies);
-    this.player.drawKillCount();
-
-    for (let zomb in this.zombies) {
-      let { x } = this.zombies[zomb];
-      
-      if (this.zombies[zomb].notCollided) {
-        if (x < this.canvas.width - 200) {
-          this.zombies[zomb].draw()
-          this.zombies[zomb].converge();
-
-          if (delta > interval) {
-            this.then = now - (delta % interval);
-            this.cars[car].animateMovement();
-          }
-
-          this.separateHorde(car, this.cars[car]);
-        } else {
-          this.cars[car].drawAttack();
-
-          if (delta > interval) {
-            this.then = now - (delta % interval);
-            this.cars[car].animateAttack();
-            this.player.health -= .3;
-          }
-        }
-      } else {
-        this.cars[car].drawDead();
-        if (delta > interval) {
-          this.then = now - (delta % interval);
-          this.cars[car].animateDead();
-        }
-      }
+    //move and display PEWs -based on typing speed
+    speedometer.setSpeed();
+    ctx.shadowBlur = 0;
+    for(let i = pew_pool.length - 1; i >= 0; i--){
+     pew_pool[i].wee();
+     pewGone(i);
     }
-
-    for (let car in this.cars) {
-      if (this.cars[car].notCollided) {
-        this.cars[car].drawText()
-      }
-    }
-
-    if (this.player.health > 0) {
-      this.player.drawHealth();
-      this.player.draw();
-      if (this.counter - this.attackTimer > 4000) {
-        this.player.attack = false;
-      }
-    } else if (this.player.health <= 0) {
-      this.player.health = 0;
-      this.player.drawHealth();
-      clearInterval(window.intervalId);
-      cancelAnimationFrame(request);
-      this.gameOver();
-    }
-  }
-
-
-  gameOver() {
-    this.canvas.removeEventListener('click', this.input.focus())
-    this.input.removeEventListener('keydown', this.handleCars);
-    this.input.removeEventListener('input', this.startTimer)
-    this.wordList.innerHTML = "";
-    this.input.value = "";
-    this.input.disabled = true;
-    this.input.style.display = "none";
-
-}
-}
+   }
+   
---
+++ js/scorekeeper.js
@@ -0,0 +1,63 @@
+let scoreKeeper = (function(){
+    let round = 0;
+    let score = 0;
+    let level = 0;
+   //bool to track typing an entire LINE without a mistake
+    let flawless = true;
+  
+    return{
+      increment: function(){
+        round += 100 + (level * 10);
+      },
+      decrement: function(){
+        if(round > 200) round -= 200;
+      },
+      draw: function(){
+      // font size set in cleanSlate
+        ctx.textAlign = 'end';
+        ctx.fillText('Score: ' + score, 780, 30);
+        if(flawless){
+         ctx.textAlign = 'center';
+        }
+        ctx.textAlign = 'start'; // this sets alignment back to default for the LINE display
+        ctx.fillText('Level ' + level + ': ' + round, 20, 30);
+        ctx.beginPath();
+        ctx.moveTo(0, 40);
+        ctx.lineTo(800, 40);
+        ctx.stroke();
+      },
+      endLevel: function(){
+        if(flawless) score += round;
+        if(level > 4) score += score; //beating the game (level 4)
+        score += round;
+        round = 0;
+        flawless = true;
+      },
+      miss: function(){
+        flawless = false;
+      },
+      isFlawless: function(){
+        return flawless;
+      },
+      getRound: function(){
+        return round;
+      },
+      getScore: function(){
+        return score;
+      },
+      getLevel: function(){
+        return level; //called by deck.march and checked for opening screen
+      },
+      newLevel: function(){
+        level++;
+        //added 'assets'
+        // to do - document.getElementById('outerEdge').style.backgroundImage = 'url("assets/level_' + level + '.gif")';
+      },
+      clear: function(){
+        round = 0;
+        score = 0;
+        level = 0;
+        flawless = true;
+      }
+    }
+  })();
